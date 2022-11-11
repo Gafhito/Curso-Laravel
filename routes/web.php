669a721c855b94a2cf1c50b92e7df580ae8c0b59
Gafhito <gonzalo.ferrari@outlook.es>
2022-11-11 15:03:12 -0300

    Primer commit

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ExpenseReportController;
+use App\Http\Controllers\ExpenseController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/hola', function () {
+    return 'Hola Mundo desde Laravel !';
+});
+
+Route::get('/json', function () {
+    return [
+        'saludo' => 'Hola',
+        'nombre' => 'Gonzalo'
+    ];
+});
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
+
+Route::resource('/expense_reports', ExpenseReportController::class);
+
+Route::get('/expense_reports/{id}/confirmDelete', [ExpenseReportController::class, 'confirmDelete']);
+
+Route::get('/expense_reports/{expense_report}/expenses/create', [ExpenseController::class, 'create']);
+
+Route::post('/expense_reports/{expense_report}/expenses', [ExpenseController::class, 'store']);
